a9e458db9906da64e44272b39d188094a53092ad
Bruno Cerqueira <brunocerqueiram@gmail.com>
2018-07-26 23:49:03 -0300

    CRUD system created, twitter gem installed.

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,25 +4,43 @@ class KeywordsController < ApplicationController
     @keywords = Keyword.all
   end
 
-  def new
+  def show
+    @keyword = Keyword.find(params[:id])
+  end
 
+  def new
+    @keyword = Keyword.new
   end
 
   def create
-
+    @keyword = Keyword.new(key_params)
+    @keyword.save
+    redirect_to @keyword
   end
 
   def edit
-
+    @keyword = Keyword.find(params[:id])
   end
 
   def update
-
+    @keyword= Keyword.find(params[:id])
+    if @keyword.update(key_params)
+      redirect_to keywords_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @keyword = Keyword.find(params[:id])
+    @keyword.destroy
+    redirect_to keywords_path
   end
 
+  private
+
+  def key_params
+    params.require(:keyword).permit(:word)
+  end
 
 end
